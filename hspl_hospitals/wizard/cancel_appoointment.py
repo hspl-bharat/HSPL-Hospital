fc9049af100e6ac4346ed088c6d71b5f724f618c
bharat <bhara@heliconia.in>
2023-04-25 18:21:34 +0530

    apr 25 tuesday

--- hspl_hospitals/wizard/cancel_appoointment.py
+++ hspl_hospitals/wizard/cancel_appoointment.py
@@ -1,12 +1,20 @@
+import datetime
 from odoo import api, fields, models
 
 class CancelAppointmentWizard(models.TransientModel):
     _name ='cancel.appointment.wizard'
     _description = 'Appointment Cancellation Wizard'
 
-    appointment_id = fields.Many2one('hspl.hospital.appointment', string="Appointment")
-    reason  = fields.Text(string="Reason")
+    @api.model
+    def default_get(self, fields):
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        res['date_cancel'] = datetime.date.today()
+        return res
 
+    appointment_id = fields.Many2one('hspl.hospital.appointment', string="Appointment")
+    reason = fields.Text(string="Reason")
+    date_cancel = fields.Date(string='Cancellation Date')
+    state = fields.Selection(related="appointment_id.status")
     def action_cancel(self):
         for rec in self:
-            rec.status = 'cancelled'
+            rec.state = 'cancelled'
